e9f40226b7434167d8cfcaf06101f0425c75247b
JAMAL-MOHAMED-JALDESA-2893 <jayhamo22@gmail.com>
2024-03-12 15:26:13 +0300

    update

--- src/app/sidenav/nav-dat.ts
+++ src/app/sidenav/nav-dat.ts
@@ -3,6 +3,26 @@ export const navbarData = [
     routeLink: 'dashboard',
     icon: 'fal fa-home',
     label: 'Dashboard'
+  },
+  {
+    routeLink: 'dashboard',
+    icon: 'fal fa-home',
+    label: 'Dashboard'
+  },
+  {
+    routeLink: 'dashboard',
+    icon: 'fal fa-home',
+    label: 'Dashboard'
+  },
+  {
+    routeLink: 'dashboard',
+    icon: 'fal fa-home',
+    label: 'Dashboard'
+  },
+  {
+    routeLink: 'dashboard',
+    icon: 'fal fa-home',
+    label: 'Dashboard'
   }
 ];
 
